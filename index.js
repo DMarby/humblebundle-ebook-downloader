76ce26687954d4b89932db9939f74b41d1804921
J. Longman <jlongman@gmail.com>
2016-12-30 16:49:34 -0500

    Skip existing files

--- index.js
+++ index.js
@@ -58,25 +58,25 @@ unirest
       var postcount = orders.length
 
       console.log("%s of %s shown", postcount, precount)
- 
+
       var options = []
-      
+
       orders.forEach(function (order) {
         options.push(order.product.human_name)
       })
-      
+
       inquirer.prompt({ type: 'list', name: 'bundle', message: 'Select a bundle to download', choices: options }, function (answers) {
-        var downloads = orders.filter(function (item) { 
-          return answers.bundle == item.product.human_name 
+        var downloads = orders.filter(function (item) {
+          return answers.bundle == item.product.human_name
         })[0].subproducts.filter(function (item) {
           return item.downloads.length
         })
-        
+
         async.eachLimit(downloads, commander.download_limit, function (download, next) {
           var human_name = striptags(download.human_name)
           var filename = (download.downloads[0].machine_name + '.' + commander.format.toLowerCase()).replace(/\.pdf \(hd\)/,'.pdf')
           var download_url = download.downloads[0].download_struct.filter(function (item) { return item.name.toLowerCase() == commander.format.toLowerCase() })
-          
+
           if (download_url.length < 1) {
             var types = []
             download.downloads[0].download_struct.forEach(function (item) {
@@ -88,23 +88,30 @@ unirest
             console.log('No download of this format found for %s (%s of %s) Formats available: %s', human_name, (i++ + 1), downloads.length, types.join(', '))
             return next()
           }
-          
-          var url = download_url[0].url.web  
-          console.log('Downloading %s (%s of %s) - %s', human_name, (i++ + 1), downloads.length, filename)
 
-          var file = fs.createWriteStream(path.resolve(commander.download_folder, filename))
-          if (download.downloads.length > 1) {
-            console.log('More than one download for %s', human_name)
-          }
 
-          https.get(url, function (response) {
-            response.pipe(file)
-            file.on('finish', function () {
-              file.close(function () {
-                next()
+          var download_path = path.resolve(commander.download_folder, filename)
+          if (fs.existsSync(download_path)) {
+            console.log('Skipping %s (%s of %s) - %s', human_name, (i++ + 1), downloads.length, filename)
+            next()
+          } else {
+            var url = download_url[0].url.web
+            console.log('Downloading %s (%s of %s) - %s', human_name, (i++ + 1), downloads.length, filename)
+
+            var file = fs.createWriteStream(download_path)
+            if (download.downloads.length > 1) {
+              console.log('More than one download for %s', human_name)
+            }
+
+            https.get(url, function (response) {
+              response.pipe(file)
+              file.on('finish', function () {
+                file.close(function () {
+                  next()
+                })
               })
             })
-          })
+      		}
         }, function (error) {
           console.log('Done!')
         })
